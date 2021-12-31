0298f86f8227305b6fc2958b9d9ae084c3f15b84
UniM0cha <solst_ice@naver.com>
2021-12-31 18:37:46 +0900

    빠뜨린것 다시

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 import { MoviesModule } from './movies/movies.module';
-import { AppController } from './app/app.controller';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [MoviesModule],
